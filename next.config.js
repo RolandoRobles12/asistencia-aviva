5d14f27842e147f2f1ae14518de57d3fd9da14d2
RolandoRobles <rolando.robles@avivacredito.com>
2025-07-24 16:46:27 -0600

    Fix Firebase workflows and configuration

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Remove output: 'export' to allow Server Actions
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      }
-    ],
-  },
+      { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' }
+    ]
+  }
 };
 
 module.exports = nextConfig;
